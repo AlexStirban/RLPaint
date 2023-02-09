d30433e2d2d145d8478e8509479409f3ac31b92c
AlexStirban <stirvaslex@gmail.com>
2023-02-09 13:15:32 +0100

    Progress on model

--- Playground/RLPaint/telesketch/envs/discrete_telesketch.py
+++ Playground/RLPaint/telesketch/envs/discrete_telesketch.py
@@ -16,19 +16,21 @@ import matplotlib.pyplot as plt
 
 
 class DiscreteTelesketchEnv(gym.Env):
-    """Discrete telesketch environment class"""
+    metadata = {"render_modes": ["image"]}
 
-    metadata = {"render_modes": ["image"]}  
-    
-    def __init__(self, ref_canvas, sim_func, segment_length=5, stroke_thickness=5, render_mode=None) -> None:
+    def __init__(self, 
+                ref_canvas, 
+                sim_func, 
+                segment_length=5, 
+                stroke_thickness=5, 
+                patch_size=(25, 25), 
+                render_mode=None) -> None:
+
         # Save ref image & sim func
         self.ref_canvas = self._to_norm_gray(ref_canvas)
         self.sim_func = sim_func
+        self._patch_size = patch_size
         self._reset_called = False
-
-        # Init inner variables
-        self._loc = None
-        self._canvas = None
 
         # Save segment params
         self.segment_length = segment_length
@@ -41,7 +43,10 @@ class DiscreteTelesketchEnv(gym.Env):
         self.observation_space = spaces.Dict(
             {
                 "loc": spaces.Box(np.array((0, 0)), np.array(self.ref_canvas.shape), dtype=int),
-                "canvas": spaces.Box(0, 255, shape=self.ref_canvas.shape, dtype=np.float32)
+                "ref": spaces.Box(0, 1, shape=self.ref_canvas.shape, dtype=np.float32),
+                "cnv": spaces.Box(0, 1, shape=self.ref_canvas.shape, dtype=np.float32),
+                "ref_patch": spaces.Box(0, 1, shape=patch_size, dtype=np.float32),
+                "cnv_patch": spaces.Box(0, 1, shape=patch_size, dtype=np.float32)
             }
         )
 
@@ -57,7 +62,15 @@ class DiscreteTelesketchEnv(gym.Env):
         return img.astype(np.float32) / 255
 
     def _get_obs(self):
-        return {"loc": self._loc, "canvas": self._canvas}
+        cnv_patch = self._compute_patch(self._canvas, self._loc, self._patch_size)
+        ref_patch = self._compute_patch(self.ref_canvas, self._loc, self._patch_size)
+
+        return {"loc": self._loc, 
+                "ref": self.ref_canvas, 
+                "cnv": self._canvas,
+                "ref_patch": ref_patch,
+                "cnv_patch": cnv_patch
+        }
     
     def _get_info(self):
         return {"sim": self.sim_func(self._canvas, self.ref_canvas)}
@@ -66,9 +79,27 @@ class DiscreteTelesketchEnv(gym.Env):
         old_sim = self.sim_func(ref_canvas, old_canvas)
         new_sim = self.sim_func(ref_canvas, new_canvas)
 
-        return 1 if new_sim > old_sim else 0
- 
-    def reset(self, loc=(0, 0), seed=None):
+        return 1 if new_sim < old_sim else 0
+    
+    def _compute_patch(self, canvas: np.ndarray, loc: np.ndarray, size: np.ndarray) -> np.ndarray:
+        # Define square patch
+        xmin = np.clip(int(loc[0] - np.floor(size[0] / 2)), 0, canvas.shape[0])
+        xmax = np.clip(int(loc[0] + np.floor(size[0] / 2)), 0, canvas.shape[0])
+        ymin = np.clip(int(loc[1] - np.floor(size[0] / 2)), 0, canvas.shape[1])
+        ymax = np.clip(int(loc[1] + np.floor(size[0] / 2)), 0, canvas.shape[1])
+
+        # Get patch
+        img = canvas[xmin:xmax, ymin:ymax]
+        padding = [
+            # Top, bottom
+            (0, size[1] - img.shape[1]),
+            # Right, left
+            (0, size[0] - img.shape[0])
+        ]
+        
+        return np.pad(img, padding, mode="constant", constant_values=1)
+
+    def reset(self, loc=(0, 0), seed=None,):
         super().reset(seed=seed)
 
         self._loc = np.array(loc)
@@ -90,7 +121,7 @@ class DiscreteTelesketchEnv(gym.Env):
 
         # Compute new states
         new_loc = self._loc + np.round(np.array([x, -y])).astype(int)
-        new_canvas = cv2.line(self._canvas, self._loc, new_loc, (0, 0, 0), self.stroke_thickness)
+        new_canvas = cv2.line(self._canvas.copy(), self._loc, new_loc, (0, 0, 0), self.stroke_thickness)
 
         # Compute rewards
         reward = self._compute_rewards(self.ref_canvas, new_canvas, self._canvas)
@@ -99,22 +130,29 @@ class DiscreteTelesketchEnv(gym.Env):
         self._loc = new_loc
         self._canvas = new_canvas
 
-        return self._get_obs(), reward, False, False, self._get_info()
+        return self._get_obs(), reward, False, self._get_info()
 
     def render(self):
         if self.render_mode == "image":
             self._plt_render()
 
     def _plt_render(self):
-        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 8))
+        fig, ax = plt.subplots(2, 2, figsize=(8, 8))
+        obs = self._get_obs()
 
-        ref = cv2.cvtColor((self.ref_canvas * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
-        gen = cv2.cvtColor((self._canvas * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        ref = cv2.cvtColor((obs["ref"] * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        cnv = cv2.cvtColor((obs["cnv"] * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        ref_patch = cv2.cvtColor((obs["ref_patch"] * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        cnv_patch = cv2.cvtColor((obs["cnv_patch"] * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
 
-        ax1.imshow(ref, cmap="gray")
-        ax2.imshow(gen, cmap="gray")
+        ax[0][0].imshow(ref, cmap="gray")
+        ax[0][1].imshow(cnv, cmap="gray")
+        ax[1][0].imshow(ref_patch, cmap="gray")
+        ax[1][1].imshow(cnv_patch, cmap="gray")
 
-        ax1.set_title("Ref. Canvas")
-        ax2.set_title("Env. Canvas")
+        ax[0][0].set_title("Ref. Canvas")
+        ax[0][1].set_title("Env. Canvas")
+        ax[1][0].set_title("Ref. Patch Canvas")
+        ax[1][1].set_title("Env. Patch Canvas")
 
-        plt.show() 
+        plt.show()
